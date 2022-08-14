31099489d029036d9e9f9b75792c704b130c6b66
Oliver Kriska <oliver@ideax.sk>
2022-08-14 17:16:42 +0200

    formater

--- lib/playwright/api_request_context.ex
+++ lib/playwright/api_request_context.ex
@@ -11,24 +11,31 @@ defmodule Playwright.APIRequestContext do
   alias Playwright.APIRequestContext
 
   @type fetch_options() :: %{
-    optional(:params) => any(),
-    optional(:method) => binary(),
-    optional(:headers) => any(),
-    optional(:postData) => any(),
-    optional(:jsonData) => any(),
-    optional(:formData) => any(),
-    optional(:multipartData) => any(),
-    optional(:timeout) => non_neg_integer(),
-    optional(:failOnStatusCode) => boolean(),
-    optional(:ignoreHTTPSErrors) => boolean()
-  }
+          optional(:params) => any(),
+          optional(:method) => binary(),
+          optional(:headers) => any(),
+          optional(:postData) => any(),
+          optional(:jsonData) => any(),
+          optional(:formData) => any(),
+          optional(:multipartData) => any(),
+          optional(:timeout) => non_neg_integer(),
+          optional(:failOnStatusCode) => boolean(),
+          optional(:ignoreHTTPSErrors) => boolean()
+        }
 
   @spec post(t(), binary(), fetch_options()) :: Playwright.APIResponse.t()
   def post(%APIRequestContext{} = request_context, url, options \\ %{}) do
-    Channel.post(request_context, :fetch, Map.merge(%{
-      url: url,
-      method: "POST"
-    }, options))
+    Channel.post(
+      request_context,
+      :fetch,
+      Map.merge(
+        %{
+          url: url,
+          method: "POST"
+        },
+        options
+      )
+    )
   end
 
   @spec body(t(), Playwright.APIResponse.t()) :: any()
